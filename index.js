0ea8c72a2c315cfa1e37e9956a43589700daafe5
SumiAkter2 <akteralisha36@gmail.com>
2023-08-02 16:56:24 +0600

    added mongodb

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynzvptg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+   
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello Klassy Missy!");
 });
